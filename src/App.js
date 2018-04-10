593bc0e9fecc600b77839abd35b84fc1c52685cc
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2018-04-10 17:33:44 +0200

    Improving example app.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,106 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
+
+import {createNotification} from './uikit/notifications/redux';
+import {
+  TopBar,
+  TopBarLeft,
+  TopBarRight,
+  TopBarBrand,
+
+  TopBarMenuButton,
+  TopBarAlertDropdown,
+} from './uikit/topbar';
+import {
+	SideBar,
+  SideBarList,
+  SideBarItemLink,
+  SideBarLink,
+  SideBarHeader,
+} from './uikit/sidebar';
+import NotificationList from './uikit/notifications/NotificationList';
+
+
+
 class App extends Component {
+
+	componentDidMount() {
+		const {dispatch} = this.props;
+		///
+		setTimeout(() => {
+			window.$.toast({
+				heading: 'Welcome to Philbert',
+				text: 'Use the predefined ones, or specify a custom position object.',
+				position: 'top-right',
+				loaderBg:'#f0c541',
+				icon: 'success',
+				hideAfter: 3500,
+				stack: 6
+			})
+			dispatch(createNotification({
+				heading: "Welcome to Philbert",
+				text: "Use the predefined ones, or specify a custom position object.",
+				level: "warning",
+				avatarSrc: "/media/philbert/img/user1.png",
+			}))
+		}, 3000)
+	}
+
 	render() {
+		const {notifications} = this.props
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<TopBar>
+					<TopBarLeft>
+						<TopBarBrand brandTitle="ASAB" brandImgSrc={logo} brandHref="index.html" />
+						<TopBarMenuButton />
+					</TopBarLeft>
+					<TopBarRight>
+					<TopBarAlertDropdown
+						iconBadgeText={notifications.length}>
+						<li>
+							<NotificationList />
+						</li>
+					</TopBarAlertDropdown>
+					</TopBarRight>
+				</TopBar>
+
+				<SideBar>
+					<SideBarHeader headerTitle="Component" />
+					<SideBarList listId="dashboard"
+											listActive="true"
+											listTitle="PRO List"
+											listIconClass="zmdi-landscape">
+						<SideBarItemLink linkHref="#" linkTitle="I am PRO" />
+						<SideBarItemLink linkHref="#" linkTitle="I am FREE" linkActive="true" />
+					</SideBarList>
+					<SideBarHeader headerTitle="Second section" headerTopLine="true" />
+					<SideBarList listId="commerce"
+											listTitle="FREE List"
+											listIconClass="zmdi-shopping-basket"
+											listLabelClass="label-success"
+											listLabelTitle="150">
+						<SideBarItemLink linkHref="#" linkTitle="You are PRO" />
+						<SideBarItemLink linkHref="#" linkTitle="You are FREE" />
+					</SideBarList>
+					<SideBarLink linkHref="#"
+											linkIconClass="zmdi-shopping-basket"
+											linkTitle="Outsider"
+											linkLabelClass="label-warning"
+											linkLabelTitle="9" />
+				</SideBar>
 			</div>
 		);
 	}
+
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		notifications: state.notifications.notifications,
+	}
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { notifications } from './uikit/notifications/redux' ;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+	combineReducers({
+		notifications
+	}),
+);
+
+ReactDOM.render((
+	<Provider store={store}>
+		<App />
+	</Provider>
+), document.getElementById('root'));
 registerServiceWorker();
