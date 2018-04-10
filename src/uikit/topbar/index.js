2880537b301d17cc2ab8b69e44992eb73cd172d5
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2018-04-10 17:32:20 +0200

    Exporting sidebar and topbar components in modules' index.js

---
+++ src/uikit/topbar/index.js
@@ -0,0 +1,15 @@
+import TopBar from './TopBar';
+import TopBarLeft from './TopBarLeft';
+import TopBarRight from './TopBarRight';
+import TopBarBrand from './TopBarBrand';
+import TopBarMenuButton from './TopBarMenuButton';
+import TopBarAlertDropdown from './TopBarAlertDropdown';
+
+export {
+	TopBar,
+	TopBarLeft,
+	TopBarRight,
+	TopBarBrand,
+	TopBarMenuButton,
+	TopBarAlertDropdown,
+}
