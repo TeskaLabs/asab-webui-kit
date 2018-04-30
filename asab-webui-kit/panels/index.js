9c5155d42625ee918c173e778d014d9de4f150c4
Jan Pišl <jan.pisl@teskalabs.com>
2018-04-30 11:47:29 +0200

    Remove obsolete imports and exports.

--- asab-webui-kit/panels/index.js
+++ asab-webui-kit/panels/index.js
@@ -5,37 +5,15 @@ import Panel from './Panel';
 import PanelTitle from './PanelTitle';
 import PanelHeading from './PanelHeading';
 import Label from './Label';
-import ColoredPanelLeftHeading from './ColoredPanels/ColoredPanelLeftHeading';
 import PanelBody from './PanelBody';
-import PanelWithBorder from './PanelWithBorder'
-
-import PanelInfo from './ColoredPanels/PanelInfo';
-import PanelWarning from './ColoredPanels/PanelWarning';
-import PanelDanger from './ColoredPanels/PanelDanger';
-import PanelSuccess from './ColoredPanels/PanelSuccess';
-import PanelPrimary from './ColoredPanels/PanelPrimary';
-import PanelInverse from './ColoredPanels/PanelInverse';
-
-
 
 
 export {
-    PageWrapper,
-    HeadingRow,
-    Panel,
-    PanelTitle,
-    PanelHeading,
-    Label,
-    PanelBody,
-    PanelWithBorder,
-    ColoredPanelLeftHeading,
-
-    PanelInfo,
-    PanelWarning,
-    PanelDanger,
-    PanelSuccess,
-    PanelPrimary,
-    PanelInverse,
-
-
+	PageWrapper,
+	HeadingRow,
+	Panel,
+	PanelTitle,
+	PanelHeading,
+	Label,
+	PanelBody,
 };
